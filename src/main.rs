4ea4d2fc4d377a2fac49d6512baaf8d9b5f4da4b
KennethBaoLe <kennethble@gmail.com>
2026-04-09 22:17:34 -0400

    Pulling each column as a typed array

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 // Bring all Polars types and traits into scope 
 use polars::prelude::*;
+use std::collections::HashMap;
+use polars::prelude::*;
 
 // Return type lets us use `?` for error handling — if anything fails, it exits with the error
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -58,10 +60,34 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let one = ga_r.head(Some(1));
     println!("gaba_avg:  {:?}", one.column("gaba_avg")?);
     println!("ach_avg:   {:?}", one.column("ach_avg")?);
+    
+    // building the hashmap of neuron connections
+    // sender, recipient, weight if fired last timestep (syn_count x sign)
+    // source_neuron_id, weight 
 
-    let pre = ga_r.column("pre_pt_root_id")?.n_unique()?;
+    let counts = ga_r.column("syn_count")?.i64()?; // getting i64 column as a ChunkedArray
+    println!("Counts: {:?}", counts);
+    let first_value = counts.get(0);
+    println!("First syn_count: {:?}", first_value);
+
+    let pre = ga_r.column("pre_pt_root_id")?.i64()?;
     let post = ga_r.column("post_pt_root_id")?.n_unique()?;
+    let gaba = ga_r.column("gaba_avg")?.f64()?;
+    let ach = ga_r.column("ach_avg")?.f64()?;
+    let glut = ga_r.column("glut_avg")?.f64()?;
+
+    /*
+    let neurop = ga_r.column("neuropil")?.i64()?;
+    let oct = ga_r.column("oct_avg")?.f64()?;
+    let ser = ga_r.column("ser_avg")?.f64()?;
+    let da = ga_r.column("da_avg")?.f64()?;
+    */
+    
     println!("Pre neurons: {pre}, Post Neurons: {post}");
     
+    for col in df.iter() {
+        println!("Column name: {}", col.name());
+    }
+
     Ok(())
 }
