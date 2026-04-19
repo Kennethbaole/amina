baff71426b2e7178d961c7538f28f8ee14a3024a
KennethBaoLe <kennethble@gmail.com>
2026-04-18 21:14:49 -0400

    Filter Region function

---
+++ src/data.rs
@@ -0,0 +1,21 @@
+// loading and filtering the feather file
+
+use polars::prelude::*;
+
+// path: &str -> takes a string slice as input (borrowed string)
+pub fn load_connectome(path: &str) -> Result<DataFrame, Box<dyn std::error::Error>> {
+    let file = std::fs::File::open(path)?;
+    let df = IpcReader::new(file).finis()?;
+    Ok(df)
+}
+
+// filtering by region
+pub fn filter_region(df: &DataFrame, region: &str) -> Result<DataFrame, PolarsError> {
+    let filtered = df
+        .clone()
+        .lazy()
+        .filter(col("region").eq(lit(region)))
+        .collect()?;
+
+    Ok(filtered)
+}
--- src/main.rs
+++ src/main.rs
@@ -3,51 +3,17 @@ use polars::prelude::*;
 use std::collections::HashMap;
 use polars::prelude::*;
 
-// Return type lets us use `?` for error handling — if anything fails, it exits with the error
+mod data; // tells rust to "look for src/data.rs"
+mod graph; 
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Path to the FlyWire connectome file, relative to where we run `cargo run`
-    let path = "data/proofread_connections_783.feather";
-
-    // Open the file — returns a std::fs::File handle (or errors if file not found)
-    let file = std::fs::File::open(path)?;
-
-    // IpcReader reads Arrow IPC format (which is what .feather files are)
-    // .finish() actually reads and parses the file into a DataFrame
-    let df = IpcReader::new(file).finish()?;
-
-    // Print the first 5 rows — Some(5) because the method takes an Option<usize>
-    /*
-    println!("{}", df.head(Some(5)));
-    println!("Columns: {:?}", df.get_column_names());
+    let df = data::load_connectome("data/proofread_connections_783.feather")?;
     println!("Shape: {:?}", df.shape());
-    */
 
-    /*
-    // unique neurons:
-    let pre = df.column("pre_pt_root_id")?.n_unique()?;
-    println!("Unique pre neurons: {pre}");
-    let post = df.column("post_pt_root_id")?.n_unique()?;
-    println!("Unique post neurons: {post}");
+    let ga_r = data::filter_region(&df, "GA_R")?;
 
-    // printing unique values in neuropil
-    let neurop = df.column("neuropil")?.n_unique()?;
-    println!("Unique neuropils: {neurop}");
-    println!("{:?}", df.column("neuropil")?.unique()?);
+    let graph = graph::build_graph(&ga_r)?;
 
-    // filtering SEZ_R
-    let small = df.clone().lazy()
-        .filter(col("neuropil").eq(lit("SEZ_R")))
-        .collect()?;
-    println!("SEZ_R shaoeL {:?}", small.shape());
-
-    for region in ["PB", "AL_L", "AL_R", "GA_R"] {
-        let subset = df.clone().lazy()
-            .filter(col("neuropil").eq(lit(region)))
-            .collect()?;
-        println!("{}: {} connections", region, subset.shape().0);
-    }
-
-    */
     // filter to ga_z
     let ga_r = df.clone().lazy()
         .filter(col("neuropil").eq(lit("GA_R")))
